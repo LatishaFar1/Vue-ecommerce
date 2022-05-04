8649a14fe3b9133708105dfd93b5948ebaa1cb56
Tish Faroul <lfaroul14@gmail.com>
2022-05-03 20:52:18 -0400

    edited shop page

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,82 +3,9 @@
     <v-app>
     <v-container fluid>
         <v-card color="#000" height="150px" tile flat class="d-flex align-center justify-center" dark>
-              <v-row>
-        <v-col cols="12" sm="12">
-        </v-col>
-        <v-col cols="12" sm="12">
-          <v-breadcrumbs :items="items" class="justify-center mt-n7" dark>
-            <template v-slot:divider>
-              <v-icon color="#7C92FE">mdi-chevron-right</v-icon>
-            </template>
-          </v-breadcrumbs>
-        </v-col>
-      </v-row>
+          <h2>SHOP</h2>
     </v-card>
 
-    <v-card tile class="mx-16 mt-n10 card1" color="white">
-      <v-row>
-        <v-col cols="12" sm="8" class="pr-0">
-          <v-card height="250px" tile flat color="#030706">
-            <v-row>
-              <v-col cols="12" sm="4" class="pl-8 pt-5">
-                <h3 class="white--text mt-5">welcome</h3>
-                <v-btn rounded color="#6fc9bd" dark class="px-8 mt-10" >More Info
-                  <a href="/about">
-
-                  </a> </v-btn>
-              </v-col>
-
-              <v-col cols="12" sm="8">
-                <v-img
-                  src="store logo.jpeg"
-                  max-height="250"
-                  max-width="400"
-                  contain
-                  class="mt-n6"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col cols="12" sm="3" class="mt-n6 pr-0">
-          <v-toolbar flat outlined>
-            <v-toolbar-title><strong>Filter</strong></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-divider vertical></v-divider>
-            <v-btn icon class="ml-1">
-              <v-icon color="#080c0b"> mdi-sync </v-icon>
-            </v-btn>
-          </v-toolbar>
-        </v-col>
-
-        <v-col cols="12" sm="3" class="mt-n6 px-0">
-          <v-toolbar flat outlined>
-            <v-toolbar-title>
-              SORT BY: <span>$</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon class="mr-4">
-              <v-icon color="black">mdi-menu-down</v-icon>
-            </v-btn>
-          </v-toolbar>
-        </v-col>
-
-        <v-col cols="12" sm="3" class="mt-n6 pl-0">
-          <v-toolbar flat outlined>
-            <v-btn icon class="mr-1">
-              <v-icon color="#3853D8">mdi-apps</v-icon>
-            </v-btn>
-            <v-divider vertical></v-divider>
-            <v-btn icon class="mx-1">
-              <v-icon color="grey">mdi-format-list-bulleted</v-icon>
-            </v-btn>
-            <v-divider vertical></v-divider>
-          </v-toolbar>
-        </v-col>
 
         <v-col cols="3" class="py-0 pr-0 mt-n3">
           <v-card flat outlined tile>
@@ -96,18 +23,7 @@
           </v-card>
          </v-col>
 
-          </v-row>
- 
-
-    </v-card>
-
-
-    
-
-    
-
-
-
+        
 
      <v-card
     class="mx-auto my-12"
@@ -127,19 +43,13 @@
       ></v-progress-linear>
     </template>
 
-    <v-img
-      :src="clothe.image"   width="300" height="300" contain
-    ></v-img>
+    <v-img  :src="clothe.image"   width="300" height="300" contain></v-img>
 
-    <v-card-title>{{clothe.title}}</v-card-title>
-
-
-
+    <v-card-title class="title">{{clothe.title}}</v-card-title>
 
       <div class="my-4 text-subtitle-1 green--text">
         $ {{clothe.price}}
       </div>
-
 
     <v-divider class="mx-4"></v-divider>
 
@@ -155,8 +65,10 @@
     </v-card-actions>
   </v-card>
 
+    </v-card>
 
 
+    
   </v-container>
   </v-app>
 </template>
@@ -167,23 +79,7 @@
 
 export default {
   data: () => ({
-      items: [
-          {
-              text: "HOME",
-              disabled: false,
-              href: "breadcrumbs_home",
-          },
-          {
-              text: "TOPS",
-              disabled: false,
-              href: "breadcrumbs_tops"
-          },
-           {
-              text: "BOTTOMS",
-              disabled: false,
-              href: "breadcrumbs_bottoms"
-          }
-      ],
+    
       Clothes: [
           {
               class: "pa-0",
@@ -238,7 +134,13 @@ export default {
         width: 100%;
     }
 
-    .a.v-breadcrumbs__item{
-        color: #03fcdb;
+    .title{
+      align-content: center;
+      text-align: center;
     }
+    h2{
+       color: #03fcdb;
+       font-size: 35px;
+    }
+    
 </style>
