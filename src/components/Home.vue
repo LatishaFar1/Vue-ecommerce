7ffd5131247fbb83f905d19275b96c0cf48db2f8
Tish Faroul <lfaroul14@gmail.com>
2022-05-05 20:13:12 -0400

    edited home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,20 +30,7 @@
          See my 'Projects' tab for other things I've worked on.
            <br/>
            <br/>
-           "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi velit orci, egestas eget lectus ut, tempus facilisis enim. Aliquam arcu erat, hendrerit eget purus vel, maximus gravida justo. Aliquam commodo tortor nisi, non congue nisl consectetur a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent eleifend fermentum tellus, id dignissim erat volutpat id. Donec bibendum, tortor id elementum imperdiet, leo elit fringilla velit, quis venenatis est massa ut metus. Vivamus faucibus risus eros, eu porttitor velit tempus ac. Sed eros erat, convallis a turpis in, porttitor efficitur magna. Nulla arcu augue, facilisis a sagittis id, mattis vel velit. Aliquam erat volutpat.
-
-
-
-In eu purus et magna facilisis interdum. Mauris eu leo eget elit condimentum sollicitudin. Phasellus erat libero, aliquet non est id, pharetra sodales nunc. Vivamus tellus diam, ullamcorper in tempus eu, tempus in metus. Nullam ac accumsan sapien. Maecenas mattis venenatis porta. Nunc consequat porta enim, sit amet placerat sem. Maecenas magna diam, facilisis non ante eget, tempus ornare enim. Cras sit amet molestie est, vitae maximus urna. Nam mattis magna eget mauris cursus, in mattis quam eleifend. Suspendisse sed lacus vel augue tincidunt tempor. Aliquam malesuada turpis at ipsum dictum rutrum. Pellentesque a urna ante.
-
-Vivamus egestas posuere tortor in fermentum. Nam ac massa purus. Vestibulum lectus libero, tempor sed aliquet at, posuere a tortor. Donec erat augue, pellentesque a leo at, pulvinar maximus augue. Proin at leo eu ex scelerisque tristique. Morbi fermentum sit amet est ut auctor. Suspendisse potenti.
-
-Proin dictum, ex ullamcorper lobortis ultrices, dui arcu tempor turpis, sed congue lacus dolor vehicula velit. Nam ultricies nulla at ullamcorper ultrices. Aliquam a diam arcu. Vivamus ullamcorper, ex ac bibendum vehicula, mi est consequat nisl, ut ultrices turpis dolor in metus. Aenean lorem nibh, dignissim vitae nisi vel, lacinia fringilla est. Etiam blandit nibh sed arcu semper, nec rhoncus nibh tempus. Nullam bibendum leo metus, eget iaculis arcu pulvinar non.
-
-Nam a nulla et ipsum mollis interdum. Ut tincidunt massa posuere nibh iaculis, quis efficitur turpis posuere. Pellentesque ante metus, cursus ac purus eu, bibendum volutpat velit. Pellentesque cursus mauris vitae magna vulputate tincidunt. Aliquam erat volutpat. Pellentesque at aliquam ipsum. Aenean a vehicula elit. Suspendisse auctor eu libero eget dignissim. Cras rhoncus dolor vel lacus mattis auctor. Curabitur vehicula velit eget odio condimentum mattis. Quisque tristique lobortis euismod. Praesent tellus elit, convallis ac semper sit amet, rhoncus et ligula. Fusce fringi
+     
 
 </p>
        </div>
